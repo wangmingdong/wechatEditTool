1655d9e9435642f8735abc297c17f461d82c671e
wangmingdong <wangmingdong@imzhiliao.com>
2019-03-22 15:47:00 +0800

    firstComment

---
+++ js/sq.js
@@ -0,0 +1,17 @@
+function password() { 
+    var testV = 1; 
+    // var pass1 = prompt('请输入授权码:', ''); 
+    // while (testV < 3) { 
+    //     if (!pass1) history.go(-1); 
+    //     if (pass1 == "indoorsteam") { 
+    //         alert('欢迎您回来，Whigest已经为您呈现最新的样式，谢谢使用！'); 
+    //         break; 
+    //     } 
+    //     testV += -1; 
+    //     var pass1 = prompt('抱歉！请重新输入:'); 
+    //     document.form_name.action = " "; 
+    // } 
+    // if (pass1 != "password" & testV == 3) history.go(-1); 
+    return " "; 
+} 
+document.write(password());
